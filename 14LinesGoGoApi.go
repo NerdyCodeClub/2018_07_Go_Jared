4ed1be87c0d837cec743062dea7fd92758e13f89
Bayological <paulbayo2002@hotmail.com>
2018-07-20 15:23:52 -0500

    Added logging to request for something

--- 14LinesGoGoApi.go
+++ 14LinesGoGoApi.go
@@ -1,10 +1,12 @@
 package main
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
 func GetSomething(response http.ResponseWriter, request *http.Request) {
+	log.Print("Got a request for something from " + request.Host)
 	json.NewEncoder(response).Encode("Something")
 }
 func main() {
